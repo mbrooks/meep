21390be7281d9fd6903464dfaa82b96f2f952c62
Matt Brooks <matt@msbrooks.com>
2017-09-09 10:18:32 -0500

    Tweaked the login screen

--- Components/LoginScreen.js
+++ Components/LoginScreen.js
@@ -1,5 +1,22 @@
 import React, { PropTypes } from 'react';
-import { Button, Picker, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, TextInput, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF8DC',
+  },
+  textInput: {
+    height: 40,
+    width: 300,
+    borderColor: 'black',
+    borderWidth: 1,
+    backgroundColor: 'white',
+  },
+});
 
 class LoginScreen extends React.Component {
   constructor(props) {
@@ -8,37 +25,63 @@ class LoginScreen extends React.Component {
     this.state = {
       selectedUserId: null,
       users: this.props.users,
+      username: '',
+      password: '',
     };
 
     this.handleLogin = this.handleLogin.bind(this);
   }
 
   handleLogin() {
-    if (!this.state.selectedUserId) {
+    if (!this.state.password) {
       return;
     }
 
-    const currentUser = this.state.users.find(user => user.id === this.state.selectedUserId);
+    const currentUser = this.state.users.find(user => user.name === this.state.username);
+
+    if (!currentUser) {
+      return;
+    }
+
     this.props.action(currentUser);
   }
 
   render() {
-    const items = this.state.users.map(user => (
-      <Picker.Item key={user.id} value={user.id} label={user.name} />
-    ));
-
     return (
-      <View>
-        <Picker
-          selectedValue={this.state.selectedUserId}
-          onValueChange={itemValue => this.setState({ selectedUserId: itemValue })}
-        >
-          <Picker.Item key value label="Select User to Login" />
-
-          {items}
-
-        </Picker>
-        <Button title="Login" onPress={this.handleLogin} />
+      <View style={styles.container}>
+        <View>
+          <Image
+            resizeMode={'contain'}
+            style={{ width: 200, height: 200 }}
+            source={{ uri: 'https://s3.amazonaws.com/comedy-hackathon/images/meepLogo.jpg' }}
+          />
+        </View>
+        <View>
+          <View>
+            <Text>Username:</Text>
+          </View>
+          <View>
+            <TextInput
+              style={styles.textInput}
+              onChangeText={username => this.setState({ username })}
+              value={this.state.username}
+            />
+          </View>
+          <View>
+            <Text>Password:</Text>
+          </View>
+          <View>
+            <TextInput
+              style={styles.textInput}
+              onChangeText={password => this.setState({ password })}
+              secureTextEntry
+              value={this.state.password}
+            />
+          </View>
+          <View>
+            <Button title="Login" onPress={this.handleLogin} />
+          </View>
+        </View>
       </View>
     );
   }
